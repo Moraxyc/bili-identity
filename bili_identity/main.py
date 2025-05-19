5a7042b78dee86867740ed87a68cc7cc1e6a8121
Moraxyc <i@qaq.li>
2025-05-19 23:48:51 +0800

    feat(bili_identity/main.py): add initial FastAPI application setup for Bili Identity service
    This commit introduces the initial setup of a FastAPI application for the Bili Identity service. The application includes basic configuration, middleware for CORS, and a root endpoint that returns a welcome message. Additionally, it sets up logging and a lifespan manager for database initialization (currently commented out). The code is structured to be easily extendable with OIDC and admin routes in the future.

---
+++ bili_identity/main.py
@@ -0,0 +1,64 @@
+import logging
+from contextlib import asynccontextmanager
+
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from bili_identity import __version__
+
+# from .api import admin, oidc
+from .config import config
+
+logger = logging.getLogger(__name__)
+
+# 创建app实例
+app = FastAPI(
+    title="Bili Identity",
+    description="B站账号认证 OpenID Connect 兼容层",
+    version=__version__,
+)
+
+# TODO: 改为配置文件里设置，以达到生产可用目的
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# TODO: 完成路由
+# 注册 OIDC 和管理端路由
+# app.include_router(oidc.router, prefix="", tags=["oidc"])
+# app.include_router(admin.router, prefix="/admin", tags=["admin"])
+
+
+@app.get("/")
+def read_root():
+    return {"msg": "Welcome to Bili Identity OpenID Connect Service."}
+
+
+from .db import init_db
+
+
+@asynccontextmanager
+async def lifespan(_):
+    # 应用启动时
+    # TODO: 完成数据库
+    # await init_db()
+    yield
+
+
+def main() -> None:
+    logging.basicConfig(level=getattr(logging, config.log.level))
+    uvicorn.run(
+        "bili_identity.main:app",
+        host=config.server.host,
+        port=config.server.port,
+        reload=config.server.reload,
+    )
+
+
+if __name__ == "__main__":
+    main()
