66aad20ae32faca99de1af30757b5d3814a59db6
Moraxyc <i@qaq.li>
2025-05-20 20:57:51 +0800

    refactor(bili_identity/__init__.py): update version file handling to support executable creation with PyInstaller

--- bili_identity/__init__.py
+++ bili_identity/__init__.py
@@ -1,8 +1,16 @@
 import os
+import sys
 
-_here = os.path.dirname(__file__)
 
-with open(os.path.join(_here, "VERSION")) as f:
+def resource_path(relative_path):
+    if hasattr(sys, "_MEIPASS"):
+        base_path = getattr(sys, "_MEIPASS")
+    else:
+        base_path = os.path.dirname(__file__)
+    return os.path.join(base_path, relative_path)
+
+
+with open(resource_path("VERSION")) as f:
     __version__ = f.read().strip()
 
 __all__ = ["__version__"]
