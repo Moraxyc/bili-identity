7dfdb2df71a55e007eb8bbfa0728b07319da982d
Moraxyc <i@qaq.li>
2025-05-22 20:53:39 +0800

    feat: add nickname and avatar_url fields to User model for additional user profile information

--- bili_identity/models/user.py
+++ bili_identity/models/user.py
@@ -9,7 +9,12 @@ class User(Base):
     __tablename__ = "users"
 
     uid = Column(String, primary_key=True, index=True, comment="B站UID")
-    is_verified = Column(Boolean, default=False, comment="是否已通过验证")
+    nickname = Column(String, nullable=True, comment="昵称, 无法实时更新")
+    avatar_url = Column(
+        String,
+        nullable=True,
+        comment="用户头像的链接，B站应该不会让其失效",
+    )
     created_at = Column(
         DateTime(timezone=True), default=lambda: datetime.now(timezone.utc)
     )
