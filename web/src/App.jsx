6a39f4ff3cddec5c72038a3b5793fb5bcefee1dc
LinRan <i@ranawa.com>
2025-05-29 23:47:21 +0800

    ✨ feat: 添加web界面

---
+++ web/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SendCode from './code';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="bili-container">
+        <SendCode />
+      </div>
+    </div>
+  )
+}
+export default App;
